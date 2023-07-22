c0102aa19a0d9d29381d55100ba62372f84dfd22
Miya784 <9burhanjehloh@gmail.com>
2023-07-23 02:19:09 +0700

    add bluetooth

--- pages/screen/HomeScreen.js
+++ pages/screen/HomeScreen.js
@@ -1,12 +1,14 @@
-import React,{useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import dayjs from 'dayjs';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet, FlatList} from 'react-native';
+import BleManager from 'react-native-ble-manager';
+import {Button} from 'native-base';
+import {requestLocationPermission} from '../../permission';
 //import { Appbar } from 'react-native-paper';
 
+const HomeScreen = ({navigation}) => {
+  const [currentTime, setCurrentTime] = useState('');
 
-const HomeScreen = ({ navigation }) => {
-  const [currentTime,setCurrentTime] = useState('')
-  
   useEffect(() => {
     // Update the time every second
     const interval = setInterval(() => {
@@ -16,25 +18,37 @@ const HomeScreen = ({ navigation }) => {
     // Clean up the interval when the component is unmounted
     return () => clearInterval(interval);
   }, []); // Empty dependency array ensures the effect runs only once on mount
-  
-
 
   const handlePress = () => {
     navigation.navigate('Next');
   };
+  ///////////////////////////////////////////////////////////////////////////////
+  const [devices, setDevices] = useState([]);
 
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
+  useEffect(() => {
+    BleManager.start({showAlert: false});
+    BleManager.enableBluetooth().then(() =>
+      console.log('Bluetooth is enabled'),
+    );
+    return () => {
+      BleManager.stopScan();
+    };
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.time}> {currentTime}</Text>
       <TouchableOpacity onPress={handlePress} style={styles.button}>
         <Text style={styles.buttonText}>Coral Of</Text>
-        <Text style={styles.TextSee}>      See</Text>
+        <Text style={styles.TextSee}> See</Text>
       </TouchableOpacity>
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
@@ -42,19 +56,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  
+
   button: {
     backgroundColor: '#a49592',
     padding: 15,
     borderRadius: 30,
     width: 200,
   },
-  
-  time:{
-    fontSize:23,
+
+  time: {
+    fontSize: 23,
     color: '#ff847c',
     paddingBottom: 20,
-
   },
 
   buttonText: {
@@ -64,7 +77,7 @@ const styles = StyleSheet.create({
   TextSee: {
     fontSize: 50,
     color: '#fff',
-  }
+  },
 });
 
 export default HomeScreen;
